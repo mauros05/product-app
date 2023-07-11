bef04fe33209b28cf3b0ba0a4529ea2d554ad49e
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2023-07-11 17:51:55 -0600

    Update navbar style

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,17 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["menu"]
 
-  openMenu() {
-    this.menuTarget.classList.remove("hidden")
-    this.menuTarget.classList.add("animate-slideIn")
-  }
-
-  closeMenu() {
-    this.menuTarget.classList.remove("animate-slideIn")
-    this.menuTarget.classList.add("animate-slideOut")
-    setTimeout(() => {
-      this.menuTarget.classList.add("hidden")
-      this.menuTarget.classList.remove("animate-slideOut")
-    }, 300) // Ajusta el valor según la duración de la animación en milisegundos
+  toggleMenu() {
+    this.menuTarget.classList.toggle("hidden")
   }
 }
